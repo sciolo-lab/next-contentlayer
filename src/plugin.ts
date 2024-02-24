37eb5ebd37f8eca7a861af3aa4432f196200c3f6
MH Sagor <mhsagor91@gmail.com>
2024-02-25 00:59:24 +0600

    Fix errors

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,22 +1,22 @@
-import '@contentlayer-temp/utils/effect/Tracing/Enable'
+import "contentlayer/utils/effect/Tracing/Enable";
 
-import * as core from '@contentlayer-temp/core'
-import { errorToString } from '@contentlayer-temp/utils'
-import { E, OT, pipe, S, T } from '@contentlayer-temp/utils/effect'
-import type { WebpackOptionsNormalized } from 'webpack'
+import * as core from "@contentlayer/core";
+import { errorToString } from "@contentlayer/utils";
+import { E, OT, pipe, S, T } from "@contentlayer/utils/effect";
+import type { WebpackOptionsNormalized } from "webpack";
 
-import { checkConstraints } from './check-constraints.js'
+import { checkConstraints } from "./check-constraints.js";
 
 export type NextPluginOptions = {
-  configPath?: string | undefined
-}
+  configPath?: string | undefined;
+};
 
 /** Seems like the next.config.js export function might be executed multiple times, so we need to make sure we only run it once */
-let contentlayerInitialized = false
+let contentlayerInitialized = false;
 
 const runContentlayerDev = async ({ configPath }: NextPluginOptions) => {
-  if (contentlayerInitialized) return
-  contentlayerInitialized = true
+  if (contentlayerInitialized) return;
+  contentlayerInitialized = true;
 
   await pipe(
     core.getConfigWatch({ configPath }),
@@ -25,45 +25,37 @@ const runContentlayerDev = async ({ configPath }: NextPluginOptions) => {
     S.chainSwitchMapEitherRight((config) => core.generateDotpkgStream({ config, verbose: false, isDev: true })),
     S.tap(E.fold((error) => T.log(errorToString(error)), core.logGenerateInfo)),
     S.runDrain,
-    runMain,
-  )
-}
+    runMain
+  );
+};
 
 const runContentlayerBuild = async ({ configPath }: NextPluginOptions) => {
-  if (contentlayerInitialized) return
-  contentlayerInitialized = true
+  if (contentlayerInitialized) return;
+  contentlayerInitialized = true;
 
   await pipe(
     core.getConfig({ configPath }),
     T.chain((config) => core.generateDotpkg({ config, verbose: false })),
     T.tap(core.logGenerateInfo),
-    OT.withSpan('next-contentlayer:runContentlayerBuild'),
-    runMain,
-  )
-}
+    OT.withSpan("next-contentlayer:runContentlayerBuild"),
+    runMain
+  );
+};
 
-const runMain = core.runMain({ tracingServiceName: 'next-contentlayer', verbose: process.env.CL_DEBUG !== undefined })
+const runMain = core.runMain({ tracingServiceName: "next-contentlayer", verbose: process.env.CL_DEBUG !== undefined });
 
-export const runBeforeWebpackCompile = async ({
-  mode,
-  pluginOptions,
-  devServerStartedRef,
-}: {
-  mode: WebpackOptionsNormalized['mode']
-  pluginOptions: NextPluginOptions
-  devServerStartedRef: { current: boolean }
-}) => {
-  const isNextDev = mode === 'development'
-  const isBuild = mode === 'production'
+export const runBeforeWebpackCompile = async ({ mode, pluginOptions, devServerStartedRef }: { mode: WebpackOptionsNormalized["mode"]; pluginOptions: NextPluginOptions; devServerStartedRef: { current: boolean } }) => {
+  const isNextDev = mode === "development";
+  const isBuild = mode === "production";
 
-  const { configPath } = pluginOptions
+  const { configPath } = pluginOptions;
 
   if (isBuild) {
-    checkConstraints()
-    await runContentlayerBuild({ configPath })
+    checkConstraints();
+    await runContentlayerBuild({ configPath });
   } else if (isNextDev && !devServerStartedRef.current) {
-    devServerStartedRef.current = true
+    devServerStartedRef.current = true;
     // TODO also block here until first Contentlayer run is complete
-    runContentlayerDev({ configPath })
+    runContentlayerDev({ configPath });
   }
-}
+};
